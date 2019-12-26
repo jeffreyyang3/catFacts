0c215c40c559614044e4e01824370eb3d479e799
jeffreyyang3 <jeffreyhasmail@gmail.com>
2019-12-25 21:38:31 -0800

    stored procedure

--- create.sql
+++ create.sql
@@ -19,7 +19,7 @@ INSERT INTO users (username, phonenumber, cellProvider)
   VALUES ('Rachel', '6268183111', 'Verizon'), ('Jeffrey', '6268182618', 'Verizon'), ('Chris', '5109184345', 'Verizon');
 
 CREATE TABLE facts (
-  factId serial PRIMARY KEY,
+  factId serial PRIMARY KEY NOT NULL,
   createDate timestamp DEFAULT CURRENT_TIMESTAMP,
   fact varchar(100)
 );
@@ -28,6 +28,6 @@ CREATE TABLE texts (
   sendDate timestamp DEFAULT CURRENT_TIMESTAMP,
   textId serial PRIMARY KEY,
   userid integer REFERENCES users (userId),
-  factid integer REFERENCES facts (factId)
+  factid integer NOT NULL REFERENCES facts (factId)
 );
 
---
+++ getNewFact.pgsql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE FUNCTION getNewFact (theUserId integer)
+  RETURNS varchar
+  AS $$
+DECLARE
+  validFact varchar;
+  validFactID integer;
+BEGIN
+  SELECT
+    fact,
+    factID INTO validFact,
+    validFactID
+  FROM
+    facts
+  WHERE
+    factID NOT IN (
+      SELECT
+        facts.factID
+      FROM
+        facts
+        INNER JOIN texts ON texts.factid = facts.factid
+        INNER JOIN users ON texts.userid = theUserID)
+  LIMIT 1;
+  INSERT INTO texts (factID, userid)
+  VALUES (validFactID, theUserID);
+  RETURN validFact;
+END;
+$$
+LANGUAGE plpgsql;
+
